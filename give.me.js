f6d67a241deb6ba6ffcde0cf7a7e1b2ce43db839
pazguille <guille87paz@gmail.com>
2011-11-04 00:25:10 -0300

    Add new examples and add document.head polyfill

--- give.me.js
+++ give.me.js
@@ -6,55 +6,55 @@
 
 	var giveme = (function() {
 
-		var callback = function (call) {
-				// Do something here.
-					call();
-				},
-				createScript = function (url, call, data) {
-						var script = document.createElement("script");
-						script.type = "text/javascript";
+		if (!document.head) {
+			document.head = document.getElementsByTagName("head")[0];
+		}
+		
+		var createScript = function (url, fn, src) {
+					var script = document.createElement("script");
+					script.type = "text/javascript";
 
-						if (script.readyState) {  // IE sucks!
-							script.onreadystatechange = function () {
-								if (script.readyState == "loaded" || script.readyState == "complete") {
-									script.onreadystatechange = null;
-									if (url == data[data.length-1]) {
-										callback(call);
-									}
+					if (script.readyState) {  // IE sucks!
+						script.onreadystatechange = function () {
+							if (script.readyState == "loaded" || script.readyState == "complete") {
+								script.onreadystatechange = null;
+								if (url == src[src.length-1]) {
+									fn();
 								}
-							};
-						} else {  // Others browsers
-							script.onload = function() {
-								if (url == data[data.length-1]) {
-									console.log("termine!");
-									callback(call);
-								}
-							};
-						}
-
-						script.src = url;
-						document.body.appendChild(script);	
-				},
-				
-				core = {
-					version: "0.1",
-					css: function () {},
-					js: function (urls, call) {
-						var data = urls;
-						if (typeof urls === "string") {
-								data = [urls]; // force an array
-						}
-
-						var i = 0, len = data.length;
-						for (i; i < len; i += 1) {
-							(function () {
-								createScript(data[i], call, data);
-							}(i, call));
-						}
-
-						return giveme;
+							}
+						};
+					} else {  // Others browsers
+						script.onload = function() {
+							if (url == src[src.length-1]) {
+								console.log("termine!");
+								fn();
+							}
+						};
 					}
-				};
+
+					script.src = url;
+					document.body.appendChild(script);	
+			},
+			
+			core = {
+				version: "0.1",
+				css: function () {},
+				js: function (urls, fn) {
+					var src = urls;
+					if (typeof urls === "string") {
+							src = [urls]; // force an array
+					}
+
+					var i = 0, len = src.length;
+					for (i; i < len; i += 1) {
+						(function (i) {
+							createScript(src[i], fn, src);
+						}(i, fn));
+					}
+
+					return this;
+				}
+			};
 
 		return core;
 
