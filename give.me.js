c6ef7e702cdee820a6cee1045ae6ebdd0ed38646
pazguille <guille87paz@gmail.com>
2011-11-13 15:51:37 -0300

    Add CSS functionality

--- give.me.js
+++ give.me.js
@@ -9,49 +9,79 @@
 		if (!document.head) {
 			document.head = document.getElementsByTagName("head")[0];
 		}
+
+		var sources = [],
 		
-		var createScript = function (url, fn, src) {
-					var script = document.createElement("script");
-					script.type = "text/javascript";
+			index = 0,
 
-					if (script.readyState) {  // IE sucks!
-						script.onreadystatechange = function () {
-							if (script.readyState == "loaded" || script.readyState == "complete") {
-								script.onreadystatechange = null;
-								if (url == src[src.length-1]) {
-									fn();
-								}
-							}
-						};
-					} else {  // Others browsers
-						script.onload = function() {
-							if (url == src[src.length-1]) {
-								console.log("termine!");
-								fn();
-							}
-						};
-					}
-
-					script.src = url;
-					document.body.appendChild(script);	
+			static = {
+				"js": {
+					"tag": "script",
+					"type": "text/javascript",
+					"src": "src"
+				},
+				"css": {
+					"tag": "link",
+					"type": "text/css",
+					"src": "href",
+					"rel": "stylesheet"
+				}
 			},
-			
+
+			configSource = function (src, sources, fn, type) {
+				if (typeof src === "string") {
+					src = [src]; // force an array
+				}
+				sources = src;
+				(function () {
+					getSource(src[index], sources, fn, type);
+				}(sources, fn));
+			},
+		
+			getSource = function (src, sources, fn, type) {
+				var tag = document.createElement(static[type].tag);
+				tag.type = static[type].type;
+				if (type === "css") {
+					tag.rel = static[type].rel;
+				}
+
+				if (tag.readyState) {  // IE sucks!
+					tag.onreadystatechange = function () {
+						if (tag.readyState == "loaded" || tag.readyState == "complete") {
+							tag.onreadystatechange = null;
+							if (src == sources[sources.length-1]) {
+								fn();
+							} else {
+								index += 1;
+								getSource(sources[index], sources, fn, type);
+							}
+
+						}
+					};
+
+				} else {  // Others browsers
+					tag.onload = function() {
+						if (src == sources[sources.length-1]) {
+							fn();
+						} else {
+							index += 1;
+							getSource(sources[index], sources, fn, type);
+						}
+					};
+				}
+
+				tag[static[type]["src"]] = src;
+				document.head.appendChild(tag);
+			},
+		
 			core = {
 				version: "0.1",
-				css: function () {},
-				js: function (urls, fn) {
-					var src = urls;
-					if (typeof urls === "string") {
-							src = [urls]; // force an array
-					}
-
-					var i = 0, len = src.length;
-					for (i; i < len; i += 1) {
-						(function (i) {
-							createScript(src[i], fn, src);
-						}(i, fn));
-					}
-
+				js: function (src, fn) {
+					configSource(src, sources, fn, "js")
+					return this;
+				},
+				css: function (src, fn) {
+					configSource(src, sources, fn, "css")
 					return this;
 				}
 			};
